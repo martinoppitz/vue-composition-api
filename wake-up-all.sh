145b7ee6d3db0ddf14e6db9868be8ee327eab081
Martin <6279703+deleonio@users.noreply.github.com>
2021-08-22 13:41:53 +0200

    Add SolidJS

--- wake-up-all.sh
+++ wake-up-all.sh
@@ -6,6 +6,7 @@ bash ./wake-up.sh inferno $1
 bash ./wake-up.sh lit-element $1
 bash ./wake-up.sh preact $1
 bash ./wake-up.sh react $1
+bash ./wake-up.sh solid $1
 bash ./wake-up.sh svelte $1
 bash ./wake-up.sh vanilla $1
 bash ./wake-up.sh vue $1
